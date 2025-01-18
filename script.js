4e4b592a1e79ca4910eab2dfdde97841eecaa998
giselaimera <imeragisela@gmail.com>
2025-01-18 17:56:41 +0100

    Three buttons created for each selection, respective divs for result and score are created

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+ //score constrain
+ const SCORE_MAX=5;
+ //score variables for both players
+ let humanScore = 0;
+ let computerScore = 0;
+ //creating a button for each selection and adding a label to each one
+ const rock=document.createElement("button");
+ rock.textContent="Rock";
+ const paper=document.createElement("button");
+ paper.textContent="Paper";
+ const scissors=document.createElement("button");
+ scissors.textContent="Scissors";
+//creating a div for displaying results/score
+const result=document.createElement("div");
+const score=document.createElement("div")
  //using Math.floor and Math.random to get the computer choice
  function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
@@ -10,60 +25,67 @@
         return "Scissors";
     }
   }
-  //getting the user input, making sure it is accepted even if uppercase or mixed
-  function getHumanChoice() {
-    let humanChoice = prompt("Enter Rock, Paper, or Scissor :")
-      .trim()
-      .toLowerCase();
-    switch (humanChoice) {
-      case "rock":
-        return "Rock";
-      case "paper":
-        return "Paper";
-      case "scissors":
-        return "Scissors";
-      default:
-        return "Invalid Choice!";
-    }
+  function updateScore(){
+    score.textContent=`You: ${humanScore}, Computer: ${computerScore}`;
   }
-
-  function playGame() {
-    //score variables for both players
-    let humanScore = 0;
-    let computerScore = 0;
+    function disableButtons(){
+      rock.disabled=true;
+      paper.disabled=true;
+      scissors.disabled=true;
+    }
     //playRound function moved inside playGame
     function playRound(humanChoice, computerChoice) {
       if (humanChoice === computerChoice) {
-        console.log("It is a tie!");
+        result.textContent="It is a tie!";
       } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-        console.log("You win. Paper beats Rock!");
+        result.textContent="You win. Paper beats Rock!";
         humanScore++;
       } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-        console.log("You win. Scissors beats Paper!");
+        result.textContent="You win. Scissors beats Paper!";
         humanScore++;
       } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-        console.log("You win. Rock beats Scissors!");
+         result.textContent="You win. Rock beats Scissors!";
         humanScore++;
       } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+         result.textContent=`You lose! ${computerChoice} beats ${humanChoice}!`;
         computerScore++;
       }
     }
     
     //deciding on the winner and displaying the scores
-    if (humanScore > computerScore) {
-      console.log(
-        `You win! Final score: You: ${humanScore}, Computer: ${computerScore}`
-      );
-    } else if (humanScore < computerScore) {
-      console.log(
-        `You lose! Final score: You: ${humanScore}, Computer: ${computerScore}`
-      );
-    } else {
-      console.log(
-        `It is a tie! Final score: You: ${humanScore}, Computer: ${computerScore}`
-      );
-    }
+    if (humanScore===SCORE_MAX || computerScore===SCORE_MAX){
+      if (humanScore > computerScore) {
+        result.textContent = `You win! Final score: You: ${humanScore}, Computer: ${computerScore}`;
+      } else if (humanScore < computerScore) {
+        result.textContent = `You lose! Final score: You: ${humanScore}, Computer: ${computerScore}`;
+      } else {
+        result.textContent = `It's a tie! Final score: You: ${humanScore}, Computer: ${computerScore}`;
+      }
+  
+    
   }
-  //calling the game function to start playing the game
-  document.getElementById("playGame").addEventListener("click", playGame);
+
+  //selecting the elements
+  const container=document.querySelector("div");
+  const body=document.querySelector("body");
+  //appending elements to the ui
+  container.append(rock,paper,scissors);
+  body.append(result,score);
+  //event listener for each button clicked
+  rock.addEventListener("click", ()=>{
+    const humanChoice="Rock";
+    const computerChoice=getComputerChoice();
+    playRound(humanChoice,computerChoice);
+  });
+  paper.addEventListener("click", ()=>{
+    const humanChoice="Paper";
+    const computerChoice=getComputerChoice();
+    playRound(humanChoice,computerChoice);
+  });
+  scissors.addEventListener("click", ()=>{
+    const humanChoice="Scissors";
+    const computerChoice=getComputerChoice();
+    playRound(humanChoice,computerChoice);
+  });
+
+
